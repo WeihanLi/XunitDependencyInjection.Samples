c25d1ae781110f2bc7fef06f03dac8f49d586fed
Weihan Li <weihanli@outlook.com>
2023-12-05 13:13:41 +0800

    fix override

--- P4_TestServer/TestServerIntegration/RedisHealthCheckService.cs
+++ P4_TestServer/TestServerIntegration/RedisHealthCheckService.cs
@@ -8,10 +8,10 @@ public class RedisHealthCheckService : AbstractDependencyService
 {
     protected override TimeSpan WorkDelay => TimeSpan.FromSeconds(3);
 
-    public override virtual Task StartingAsync(CancellationToken cancellationToken)
+    public override Task StartingAsync(CancellationToken cancellationToken)
     {
         return base.StartAsync(cancellationToken);
     }
 
-    public override virtual async Task StartAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    public override async Task StartAsync(CancellationToken cancellationToken) => Task.CompletedTask;
 }
