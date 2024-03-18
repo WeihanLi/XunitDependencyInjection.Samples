b8a0c2c3f528a0412d7929d14490eb8468ad3229
Weihan Li <weihanli@outlook.com>
2024-03-18 23:11:50 +0800

    feat: update packages and add HostApplicationBuilderStartupSample

---
+++ P5_More/MoreFeatures/HostApplicationBuilderStartupSample.cs
@@ -0,0 +1,52 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Xunit;
+using Xunit.DependencyInjection;
+
+namespace MoreFeatures;
+
+public class HostApplicationBuilderStartupSample(IRandom random)
+{
+    private static int Counter;
+    
+    [Fact]
+    public void RandomTest()
+    {
+        var value = random.GetValue(2);
+        Assert.True(value is >= 0 and <= 2);
+    }
+
+    [Theory]
+    [InlineData(null!)]
+    public void ConfigurationTest([FromServices]IConfiguration configuration)
+    {
+        Assert.NotNull(configuration);
+        Assert.Equal("World", configuration["Hello"]);
+    }
+
+    [Fact]
+    public void ConfigureTest()
+    {
+        Assert.Equal(1, Counter);
+    }
+
+    public static class Startup
+    {
+        public static void ConfigureHostApplicationBuilder(IHostApplicationBuilder hostApplicationBuilder)
+        {
+            hostApplicationBuilder.Configuration.AddInMemoryCollection(
+            [
+                new KeyValuePair<string, string?>("Hello", "World"),
+                new KeyValuePair<string, string?>("CounterInitValue", "1")
+            ]);
+            hostApplicationBuilder.Services.AddSingleton<IRandom, RandomService>();
+        }
+
+        public static void Configure(IConfiguration configuration)
+        {
+            Counter = Convert.ToInt32(configuration["CounterInitValue"]);
+        }
+    }
+}
+
